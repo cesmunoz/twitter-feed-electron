10f511a592a90883cd27c9aac92537875f60bec5
Cesar Munoz <cesar.alberto.munoz@gmail.com>
2022-12-27 17:59:25 +0100

    fix: adding rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,10 @@ module.exports = {
     'import/no-unresolved': 'error',
     // Since React 17 and typescript 4.1 you can safely disable the rule
     'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    suppressImplicitAnyIndexErrors: true,
   },
   parserOptions: {
     ecmaVersion: 2020,
